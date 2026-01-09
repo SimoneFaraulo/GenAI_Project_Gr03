6012d8ffb430fdccda33739653018b8cdc0555d4
Antonio Graziosi <tonygraziosi13@gmail.com>
2026-01-09 19:06:33 +0100

    Rimozione commenti model_factory.py, trainer.py

--- train/model_factory.py
+++ train/model_factory.py
@@ -1,18 +1,9 @@
-import os
-import sys
-
-# Importa i tuoi modelli qui
 from models.autoencoder.autoencoder import ConditionalVAE
 from models.diffusion.diffusion_model import ConditionalDiffusion
 from models.mamba.vision_mamba import VisionMambaModel
 from config.config import MODEL_TYPE
-# from gan_model import ConditionalGAN (Esempio futuro)
 
 def get_model_from_env():
-    """
-    Legge la variabile d'ambiente MODEL_TYPE e restituisce l'istanza del modello e il nome dell'esperimento.
-    Default: 'vae'
-    """
     model_type = MODEL_TYPE
     
     print(f"Factory: Initializing model type '{model_type}'...")
--- train/trainer.py
+++ train/trainer.py
@@ -1,4 +1,3 @@
-import time
 import torch
 from utility.progress_indicator import ProgressIndicator
 from utility.training_utility import save_training_checkpoint, save_snapshot
@@ -7,11 +6,6 @@ from config.config import CKP_INTERVAL, FORCE_CKP, PROGRESS_INTERVAL
 class Trainer:
     def __init__(self, model, optimizer, train_loader, dataset, device, 
                  checkpoint_manager, checkpoint_folder):
-        """
-        Args:
-            train_step_fn: Funzione che accetta (model, batch, device) e 
-                           ritorna (loss, log_dict).
-        """
         self.model = model
         self.optimizer = optimizer
         self.train_loader = train_loader
@@ -42,18 +36,14 @@ class Trainer:
             self.model.train()
             
             for i, batch in enumerate(self.train_loader):
-                # 1. Step di training specifico del modello
                 loss, log_metrics = self.model.train_step_fn(batch, self.device)
-                
-                # 2. Backpropagation generica
                 self.optimizer.zero_grad()
                 loss.backward()
 
                 torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
 
                 self.optimizer.step()
-                
-                # 3. Logging e Checkpoint periodico
+
                 progress.update(1, batch_loss=loss.item(), **log_metrics)
                 save_training_checkpoint(self.cpm, self.model, self.optimizer, self.epoch_count, force=FORCE_CKP, interval=CKP_INTERVAL)
                 
@@ -61,12 +51,10 @@ class Trainer:
                     break
             
             self.epoch_count += 1
-            
-            # 4. Snapshot di fine epoca
+
             save_snapshot(self.model, self.dataset, self.checkpoint_folder, 
                           self.epoch_count, self.device)
 
-        # Salvataggio finale forzato
         save_training_checkpoint(self.cpm, self.model, self.optimizer, 
                                  self.epoch_count, force=True)
         print("Training finished.")
